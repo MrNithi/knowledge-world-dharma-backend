291f5f042316ad43c5e764c2c6163dff5597976b
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2022-04-13 14:12:56 +0700

    Initialized

---
+++ knowledge-world-dharma-backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace knowledge_world_dharma_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ knowledge-world-dharma-backend/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace knowledge_world_dharma_backend.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public Post()
+        {
+        }
+    }
+}
